64d61f9dd12753a6279bba11ebce9e995607df43
siyaphe <siyapdje@yahoo.fr>
2024-06-13 12:54:43 -0400

    my portfolio commit

--- app.js
+++ app.js
@@ -11,3 +11,28 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab")
 }
+const sidemeu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemeu.style.right = "0";
+}
+function closemenu(){
+    sidemeu.style.right = "-200px";
+}
+
+ const scriptURL = 'https://script.google.com/macros/s/AKfycbw0lavurIwAdKrgdinQHEoWTg0REGHT1RV8hmypHOBSUxN_l_rEjceEbLtqIqLQnDD3/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent sucessfully!"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
